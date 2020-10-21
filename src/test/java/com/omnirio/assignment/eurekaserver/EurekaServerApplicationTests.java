6c6cc83359aa284fd4bda86af82a21ccc522de9c
Aniket Kalamkar <aniket.kalamkar@innovect.com>
2020-10-21 10:05:37 +0530

    first commit

---
+++ src/test/java/com/omnirio/assignment/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnirio.assignment.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
